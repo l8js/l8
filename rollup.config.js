cdb23deaade7d785e46eafb123c7403462c36ec2
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2021-06-01 13:02:49 +0200

    - enhancement: removed webpack-dependencies, added rollup

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default {
+    input: './src/l8.js',
+    output : {
+        file :'./dist/l8.rollup.js',
+        format: 'iife',
+        sourceMap: 'inline',
+        name : "l8"
+    }
+};
