97c53f9a9824e67f4e3ca8deeab940f9b312413a
ThorstenSuckow <thorsten@suckow-homberg.de>
2021-09-27 12:42:18 +0200

    - enhancement: housekeeping

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,5 @@
 export default {
     "testEnvironment": "jsdom",
     "verbose": true,
-    "moduleFileExtensions": [
-        "js",
-        "mjs",
-        "ts",
-        "tsx",
-        "json",
-        "node"
-    ],
-    "rootDir" : "./",
-    "testPathIgnorePatterns": [
-        "/node_modules/"
-    ],
     "collectCoverage": true
 };
