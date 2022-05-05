5a51b3f5f328f555ddceacc0c11e9ee1bb2d5d5c
ThorstenSuckow <thorsten@suckow-homberg.de>
2022-05-05 21:26:20 +0200

    feat: add l8.md5()

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,31 @@
+/**
+ * l8.js
+ * l8
+ * Copyright (C) 2021-2022 Thorsten Suckow-Homberg https://github.com/l8js/l8
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
 export default [{
     input: './src/l8.js',
     output : [{
@@ -5,7 +33,8 @@ export default [{
         format: 'esm',
         sourcemap: true,
         name : "default"
-    }]
+    }],
+    external: ["crypto-js/md5"]
 }, {
     input: './src/.l8js',
     output : [{
@@ -13,7 +42,8 @@ export default [{
         format: 'esm',
         sourcemap: true,
         name : "default"
-    }]
+    }],
+    external: ["crypto-js/md5"]
 }, {
     input: './src/l8.js',
     output : [{
@@ -21,5 +51,6 @@ export default [{
         format: 'umd',
         sourcemap: true,
         name : "l8"
-    }]
+    }],
+    plugins: [nodeResolve(), commonjs()]
 }];
